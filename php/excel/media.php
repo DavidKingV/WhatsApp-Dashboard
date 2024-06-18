1aab6a993c70227d7a92bb116a60b06f4e2b9e78
DavidKingV <qet135ui@hotmail.com>
2024-06-18 11:59:28 -0600

    correccion de erorres

--- php/excel/media.php
+++ php/excel/media.php
@@ -14,13 +14,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $control = new Excel();
         $responses = [];
+        $allSuccess = true;
         foreach ($phoneNumbers as $phone) {
             $fullPhone = $countryCode . $phone;
             $send = $control->sendWhatsAppMedia($message, $fullPhone, $path);
+            if (!$send) {
+                $allSuccess = false; // Si alguna solicitud falla, establece en falso
+            }
             $responses[] = array('phone' => $fullPhone, 'response' => $send);
         }
 
-        echo json_encode(array('success' => true, 'message' => 'Mensajes enviados', 'responses' => $responses));
+        $message = $allSuccess ? 'Todos los mensajes fueron enviados correctamente' : 'Algunos mensajes no pudieron ser enviados';
+        echo json_encode(array('success' => $allSuccess, 'message' => $message, 'responses' => $responses));
     } else {
         echo json_encode(array('success' => false, 'message' => 'Acción no especificada o incorrecta.'));
     }
